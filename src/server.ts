f79b19782ca7ea306b0dae656f853fcc97f450d1
Philipp Rosanka <philipp.rosanka@st.oth-regensburg.de>
2026-01-13 14:33:37 +0100

    Fix: TypeScript build and hapi-bell types

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,7 @@ import { webRoutes } from "./web-routes.js";
 import { validate as jwtValidate } from "./api/jwt-utils.js";
 import { db } from "./models/db.js";
 import { validate as accountsValidate } from "./controllers/accounts-controller.js";
+/// <reference types="@types/hapi__bell" />
 
 
 
